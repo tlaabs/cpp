292edd28b4b0b15a819b27369598f1cee6d0734d
tlaabs <tlaabs@naver.com>
2017-07-22 15:51:57 +0900

    tmp

--- EmployeeManager/EmployeeManager/EmployeeManager2.cpp
+++ EmployeeManager/EmployeeManager/EmployeeManager2.cpp
@@ -58,12 +58,65 @@ public:
 	}
 };
 
+class TemporaryWorker : public Employee
+{
+private:
+	int workTime;
+	int payPerHour;
+public:
+	TemporaryWorker(char *name, int pay) :Employee(name), workTime(0), payPerHour(pay) {}
+	void addWorkTime(int time) {
+		workTime += time;
+	}
+	int getPay() const {
+		return workTime * payPerHour;
+	}
+	void showsalaryInfo() const {
+		showYourName();
+		cout << "salary: " << getPay() << endl << endl;
+	}
+};
+
+class SalesWorker : public PermanentWorker {
+private:
+	int salesResult;
+	double bonusRatio;
+public:
+	SalesWorker(char * name, int money, double ratio) : PermanentWorker(name, money), salesResult(0), bonusRatio(ratio) {}
+	void addSalesResult(int value) {
+		salesResult += value;
+	}
+	int getPay() const {
+		return PermanentWorker::getPay() +
+			(int)(salesResult*bonusRatio);
+	}
+	void showSalaryInfo() const {
+		showYourName();
+		cout << "salary : " << getPay() << endl << endl;
+	}
+};
+
 int main() {
 	EmployeeHandler handler;
 
+	//정규직
 	handler.addEmployee(new PermanentWorker("KIM", 1000));
 	handler.addEmployee(new PermanentWorker("LEE", 1500));
-	handler.addEmployee(new PermanentWorker("JUN", 1500));
+	
+	//임시직
+	TemporaryWorker * alba = new TemporaryWorker("Jung", 700);
+	alba->addWorkTime(5);
+	handler.addEmployee(alba);
+
+	//영업직
+	SalesWorker * seller = new SalesWorker("Hong", 1000, 0.1);
+	seller->addSalesResult(7000);
+	handler.addEmployee(seller);
+
+	handler.showAllSalaryInfo();
+
+	handler.showTotalSalary();
+	return 0;
 	
 	handler.showAllSalaryInfo();
 	handler.showTotalSalary();
